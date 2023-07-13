3c6000d3c2db194f7d777f8cedc37a3337c8c280
ashish nishad <nishadashish94@gmail.com>
2023-07-14 00:24:32 +0530

    code updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role',['superadmin','company','user'])->default('user');
+            $table->integer('domain_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
